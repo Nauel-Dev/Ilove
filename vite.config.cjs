93658da52f6846f3e59ef7a148462a8f5f28ac5f
Nauel-Dev <nauelverse@gmail.com>
2025-07-01 17:17:08 +0100

    Update and redeploy project

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,9 +1,8 @@
- vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+ vite.config.cjs
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
 
-// Ensure this file is treated as an ES module by "type": "module" in package.json
-export default defineConfig({
+module.exports = defineConfig({
 plugins: [react()],
 base: './',            // ensures asset paths are correct
 build: {
